c96973d1a115351390abd0aed1caaf33e149508a
Zeke-Cachi <ezequielcachi@gmail.com>
2023-08-05 18:09:45 -0300

    se corrigió un detalle de aspecto

--- telecom-challenge-front/src/App.jsx
+++ telecom-challenge-front/src/App.jsx
@@ -3,7 +3,7 @@ import ClientTable from "./components/ClientTable";
 
 function App() {
   return (
-    <main className="flex flex-col lg:flex-row h-screen">
+    <main className="flex flex-col lg:flex-row min-h-screen">
       <SideBar />
       <ClientTable />
     </main>
