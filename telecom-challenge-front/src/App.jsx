d817a592e29a863937b29f7507743be2523dbdc0
Zeke-Cachi <ezequielcachi@gmail.com>
2023-08-05 18:02:57 -0300

    se soluciona error en alturas de componentes

--- telecom-challenge-front/src/App.jsx
+++ telecom-challenge-front/src/App.jsx
@@ -3,7 +3,7 @@ import ClientTable from "./components/ClientTable";
 
 function App() {
   return (
-    <main className="flex flex-col lg:flex-row h-full">
+    <main className="flex flex-col lg:flex-row h-screen">
       <SideBar />
       <ClientTable />
     </main>
